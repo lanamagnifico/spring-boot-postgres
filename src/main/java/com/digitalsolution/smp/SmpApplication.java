4d6a052df6b9d0eeb51dbbec19f359cb39324d04
Svetlana Shestakova <svetlana.magnifico@gmail.com>
2020-06-21 10:37:22 +0200

    Init App

---
+++ src/main/java/com/digitalsolution/smp/SmpApplication.java
@@ -0,0 +1,13 @@
+package com.digitalsolution.smp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalsolution/smp/SmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalsolution.smp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
